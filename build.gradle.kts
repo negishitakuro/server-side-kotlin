6b189263af90a886971e7ed655a920b5fe2f3435
Negishi Takuro <44861671+negishitakuro@users.noreply.github.com>
2025-01-20 00:55:52 +0900

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+dependencies {
+    implementation("io.ktor:ktor-server-core:2.0.0")
+    implementation("io.ktor:ktor-server-netty:2.0.0")
+    testImplementation("io.ktor:ktor-server-tests:2.0.0")
+}
